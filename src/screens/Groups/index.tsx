36f31b47366ed16d08f2fc9b3888a74056881386
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-31 01:14:43 -0300

    utilizando styled-components - implementing styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 export function Groups() {
   return (
-    <View>
-      <Text>Groups</Text>
-    </View>
+    <>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+    </>
   )
 }
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #000;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 32px
+`;
