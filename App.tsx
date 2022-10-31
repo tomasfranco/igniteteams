c899c8bf53a57e0144ce29d2dbeae0749367efde
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-31 00:15:17 -0300

    Primeiro Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Comecando outro App para fins de Estudos - First Commit</Text>
       <StatusBar style="auto" />
     </View>
   );
