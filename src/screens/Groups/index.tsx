dff9c788c32163766b37afad422a2619f63fedf2
Tomas Franco <tomas.f.carvalho@gmail.com>
2022-11-09 01:00:07 -0300

    Ignite - React Native(Rockeseat) - Button Component Creation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header"
 import { HighLight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups ] = useState<string[]>([]);
+
   return (  
     <Container>
      <Header />
 
      <HighLight 
         title="Turmas"
-        subtitle="Jogue com a seua turna" />
+        subtitle="Jogue com a sua turma" />
+
+    <FlatList 
+      data={groups}
+      keyExtractor={item => item}
+      renderItem={({ item }) => (
+        <GroupCard
+          title={item} 
+        />
+      )}
+      contentContainerStyle={groups.length === 0 && { flex: 1 }}
+      ListEmptyComponent={() => (
+      <ListEmpty 
+      message="Que tal cadastrar a primeira turma" />)}
+    />        
+
+    <Button title="Criar nova turma" type="SECONDARY" />    
     </Container>
+
   );
 }
